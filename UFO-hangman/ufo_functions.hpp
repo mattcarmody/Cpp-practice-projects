5b225efffeb4130c7d3ae72fbdc4d958170fb993
Matt <carmodym2@gmail.com>
2020-04-09 12:07:07 -0600

    Add UFO-hangman C++ project

---
+++ UFO-hangman/ufo_functions.hpp
@@ -0,0 +1,8 @@
+#include <vector>
+
+void display_misses(int misses);
+
+// Declare functions
+void greet();
+void endgame(std::string codeword, std::string answer);
+void display_status(std::vector<char> incorrect, std::string answer);
